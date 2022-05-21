464034ca4ae71cdb35109c6981867674ae7dcb9c
Akshat <104694870+infurnex@users.noreply.github.com>
2022-05-21 20:27:46 +0530

    Update aadhardetail.sol

--- aadhardetail.sol
+++ aadhardetail.sol
@@ -1,9 +1,12 @@
 //SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
-
+pragma solidity >=0.7.0<0.9.0;
+ 
 contract IDENTITY{
-    uint public counter = 0;
-    mapping(uint => details) public data;
+    address user;
+    constructor(){
+        user = msg.sender;
+    }
+    mapping(address => details) private data;
     struct details{
         uint _adharnumber;
         string _adharname;
@@ -11,9 +14,12 @@ contract IDENTITY{
         uint _phonenumber;
         string _walletaddress;
     }
-
+ 
     function input(uint _adharnumber,string memory _adharname,string memory _DOB,uint _phonenumber,string memory _walletaddress)public{
-        counter += 1;
-        data[counter] = details(_adharnumber,_adharname,_DOB,_phonenumber,_walletaddress);
+     data[user] = details(_adharnumber,_adharname,_DOB,_phonenumber,_walletaddress);
+    }
+    function getdetails() public view returns(uint,string memory,string memory,uint,string memory){
+        require(user == msg.sender);
+        return (data[user]._adharnumber,data[user]._adharname,data[user]._DOB,data[user]._phonenumber,data[user]._walletaddress);
     }
 }
